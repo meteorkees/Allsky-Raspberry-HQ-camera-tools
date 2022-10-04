42a7a0d7d1330b5a9020c85c3c8f53ec4d5bedb5
meteorkees <75120846+meteorkees@users.noreply.github.com>
2022-10-04 21:49:07 +0200

    Update LightConditionTool.py

--- LightConditionTool.py
+++ LightConditionTool.py
@@ -1,12 +1,11 @@
 import ephem
 from math import pi
 
-
 observer_location = ephem.Observer()
 observer_location.lat='0.0000' #latitude in decimal degrees. North = +
 observer_location.lon='0.0000' #longtitide in decimal degrees. Wast = +
 # %% these parameters are for super-precise estimates, not necessary.
-observer_location.elevation = 6 # meters
+observer_location.elevation = 6 # meters above mean sealevel
 #observer_location.pressure = 1010 # millibar
 #observer_location.temp = 12 # deg. Celcius
 
@@ -15,9 +14,11 @@ sun.compute(observer_location)
 alt = (sun.alt)
 alt_sun = float(alt /pi *180) #radians to degrees, https://rhodesmill.org/pyephem/angle
 print(alt_sun) #check results
+
 #The variable cs (current_situation) will be an integer from 0, daytime, till 4, nighttime.
 #1, 2, and 3 stand for the different twilights between daytime-nighttime-daytime.
 #This integer can be used to determine what camera configuration can be used for each of these 'light' conditions.
+
 if alt_sun >= 0:
     cs = 0 #daytime
 elif (0 > alt_sun >= -6):
