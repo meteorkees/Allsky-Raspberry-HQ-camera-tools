419abae7e62290682410c042e25c3aceb5423233
meteorkees <75120846+meteorkees@users.noreply.github.com>
2022-10-31 09:30:03 +0100

    Update LightConditionTool.py

--- LightConditionTool.py
+++ LightConditionTool.py
@@ -3,9 +3,9 @@ from math import pi
 
 observer_location = ephem.Observer()
 observer_location.lat='0.0000' #latitude in decimal degrees. North = +
-observer_location.lon='0.0000' #longtitide in decimal degrees. West = -
+observer_location.lon='0.0000' #longtitide in decimal degrees. East = +
 # %% these parameters are for super-precise estimates, not necessary.
-observer_location.elevation = 6 # meters above mean sealevel
+observer_location.elevation = 6 # meters above mean sealevel (assuming the IERS 1989 ellipsoid (flattening =1/298.257))
 #observer_location.pressure = 1010 # millibar
 #observer_location.temp = 12 # deg. Celcius
 
